2470e022c9e2d90348c0276afeabae0527c7257f
hittingnote <hittingnote@gmail.com>
2020-04-12 19:07:14 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+apt-get install gcc g++ vim git wget make net-tools bc libncurses5-dev libnuma-dev libnuma1 autoconf automake libtool build-essential cmake gcc libudev-dev libnl-3-dev libnl-route-3-dev ninja-build pkg-config valgrind python3-dev cython3 libjson-c-dev uthash-dev pandoc libsystemd-dev python
